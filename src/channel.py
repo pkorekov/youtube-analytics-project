37198f2d45d4babff9bf40176f1f104b032c4595
Pavel Korekov <pkorekov@gmail.com>
2023-07-06 21:38:32 +0500

    Commit init

--- src/channel.py
+++ src/channel.py
@@ -18,6 +18,14 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriberCount = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.viewCount = self.channel["items"][0]["statistics"]["viewCount"]
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -25,7 +33,33 @@ class Channel:
         printj(channel)
 
 
+
+    @classmethod
+    def get_service(cls):
+    # Метод для работы с YouTube API
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+
+    def to_json(self, file_name):
+    # Сохранение значений атрибутов в файл
+        channel_data = {
+            "channel_id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscriberCount": self.subscriberCount,
+            "video_count": self.video_count,
+            "viewCount": self.viewCount
+        }
+        with open(file_name, "w") as json_file:
+            json.dump(channel_data, json_file)
+
+
 def printj(dict_to_print: dict) -> None:
     """Выводит словарь в json-подобном удобном формате с отступами"""
     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
+a = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+
+# print(json.dumps(a.channel, indent=2, ensure_ascii=False))
+# print(a.title)
